bd6c492815c68710806e5307a282c712d2d784df
Vitor Vinicius Duarte Oliveira <vitor.vini@live.com>
2021-06-03 19:32:32 -0300

    Add js

---
+++ main.js
@@ -0,0 +1,36 @@
+
+var nome = prompt("Tanpri ekri non pa w");
+var agradecimento = "Mèsi pou antre nan sit"
+var dt = new Date();
+var d = (dt.getHours())
+if (d <= 11){
+    alert ("Bonjou " + nome + " byenveni.");
+} if (d <= 18) {
+    alert ("Bonswa " + nome + " byenveni.");
+} if (d <= 23) {
+    alert ("Bonswa " + nome + " byenveni.");
+    alert (agradecimento);
+}
+    
+
+
+//var idade = prompt("Qual sua idade?");
+//if (idade >= 18){
+//    alert("É maior de idade.");
+//} else {
+//    alert("É menor de idade");
+//};
+
+//console.log(lista[0].toString())
+//lista.pop();
+//lista.push ("uva");
+//console.log(lista.length)
+//console.log(lista.reverse())
+//var nome = "Vitor Oliveira";
+//var idade = 34;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+//console.log(nome)
+//console.log(idade + idade2);
